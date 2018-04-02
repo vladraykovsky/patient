4a719d189a7c60187c694a9972d46e3c6ccec4e5
vlad <vraykovsky2016@gmail.com>
2018-04-02 12:32:56 +0300

    database commit two

--- src/main/java/com/parients/config/Config.java
+++ src/main/java/com/parients/config/Config.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 
 @Configuration
@@ -12,13 +13,17 @@ public class Config {
 
     @Bean
     public DriverManagerDataSource dataSource() throws URISyntaxException {
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        String username = System.getenv("JDBC_DATABASE_USERNAME");
-        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()+"?sslmode=require";
+
         DriverManagerDataSource basicDataSource = new DriverManagerDataSource();
         basicDataSource.setUrl(dbUrl);
         basicDataSource.setUsername(username);
         basicDataSource.setPassword(password);
+
         return basicDataSource;
     }
 
